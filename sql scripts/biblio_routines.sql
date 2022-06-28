6e9cb08aea5831826e1525b833f028d061575e07
LiantsoHasina <liantsorak@gmail.com>
2022-06-28 15:06:50 +0200

    Update 21

--- sql scripts/biblio_routines.sql
+++ sql scripts/biblio_routines.sql
@@ -16,6 +16,20 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary view structure for view `livre_non_rendu`
+--
+
+DROP TABLE IF EXISTS `livre_non_rendu`;
+/*!50001 DROP VIEW IF EXISTS `livre_non_rendu`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `livre_non_rendu` AS SELECT 
+ 1 AS `dateEmp`,
+ 1 AS `dureeMax`,
+ 1 AS `titre`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Temporary view structure for view `livre_emprunte`
 --
 
@@ -31,6 +45,40 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = @saved_cs_client;
 
 --
+-- Temporary view structure for view `retardataires`
+--
+
+DROP TABLE IF EXISTS `retardataires`;
+/*!50001 DROP VIEW IF EXISTS `retardataires`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `retardataires` AS SELECT 
+ 1 AS `dateEmp`,
+ 1 AS `dureeMax`,
+ 1 AS `dateRet`,
+ 1 AS `nom`,
+ 1 AS `prenom`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `livre_non_rendu`
+--
+
+/*!50001 DROP VIEW IF EXISTS `livre_non_rendu`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `livre_non_rendu` AS select distinct `e`.`dateEmp` AS `dateEmp`,`e`.`dureeMax` AS `dureeMax`,`o`.`titre` AS `titre` from (`emprunter` `e` join `oeuvres` `o` on((`e`.`NL` = `o`.`NO`))) where (`e`.`dateRet` is null) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
 -- Final view structure for view `livre_emprunte`
 --
 
@@ -44,6 +92,24 @@ SET character_set_client = @saved_cs_client;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
 /*!50001 VIEW `livre_emprunte` AS select `o`.`titre` AS `titre`,`o`.`auteur` AS `auteur`,`a`.`nom` AS `nom`,`a`.`prenom` AS `prenom` from ((`oeuvres` `o` join `emprunter` `e` on((`e`.`NL` = `o`.`NO`))) join `adherents` `a` on((`a`.`NA` = `e`.`NA`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `retardataires`
+--
+
+/*!50001 DROP VIEW IF EXISTS `retardataires`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `retardataires` AS select distinct `e`.`dateEmp` AS `dateEmp`,`e`.`dureeMax` AS `dureeMax`,`e`.`dateRet` AS `dateRet`,`a`.`nom` AS `nom`,`a`.`prenom` AS `prenom` from (`emprunter` `e` join `adherents` `a` on((`e`.`NA` = `a`.`NA`))) where (`e`.`dateRet` is null) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -65,4 +131,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-28 10:38:37
+-- Dump completed on 2022-06-28 15:05:41
